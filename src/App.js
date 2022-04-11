fff773b3d0f7885b96de42c8328e6c38993ec407
IKI-Li <103082645+IKI-Li@users.noreply.github.com>
2022-04-11 14:22:32 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,15 +8,14 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1 classname="App-title">Welcome to React</h1>
 　　　　<body>Reactのウエルコム画面です</body>
-　　　　<p>
-          このWebsiteは以下のGithubのレポによってデプロイされる
-        </p>
         <a
           className="App-link"
           href="https://github.com/IKI-Li/react-test"
           target="_white"
           rel="noopener noreferrer"
         >
+          デプロイ情報の詳細はこちら
+        </a>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
