3d661e2616742fa833d08f753f85ca97aaa4eeab
IKI-Li <103082645+IKI-Li@users.noreply.github.com>
2022-04-11 14:30:15 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,17 +19,27 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
-
+<html>
+<head>
+<title>This is my first sample Javascript project</title>
+<script>
+function displayDate(){
+    document.getElementById("demo").innerHTML=Date();
+}
+</script>
+</head>
+<body>
+    <h1>JavaScript</h1>
+    <div id="like_button_container"></div>
+    <p id="demo">Click the button below</p>
+    <button type="button" onclick="displayDate()">Display Date</button>
+    <script src="https://unpkg.com/react@17/umd/react.development.js" crossorigin></script>
+    <script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js" crossorigin></script>
+    <script src="like_button.js"></script>
+</body>
+</html>
 export default App;
